481a6ec3b5e46871d2521277b877197e5eb83171
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-16 16:44:08 +0100

    esercizio completo

---
+++ ex-hamburger-menu-base/main.js
@@ -0,0 +1,34 @@
+$('.header-right .fas fa-bars').click(
+  function(){
+    $('.hamburger-menu').show('fast');
+  }
+);
+$('.fas fa-times').click(
+  function() {
+    $('.hamburger-menu').hide('fast');
+  }
+);
+
+
+
+
+
+
+
+
+
+
+// var hamburgerVisible = $('.hamburger-menu ');
+// console.log(hamburger);
+// hamburger.click(
+//   function () {
+//     $('.hamburger-menu').show('fast');
+//   }
+// );
+
+// var menu = $('i.fas fa-times');
+// menu.click(
+//   function () {
+//     $('div.hamburger-menu').hide('fast');
+//   }
+// );
